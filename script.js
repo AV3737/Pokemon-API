c542646999e395494f8cdefdfb106c2d20e8f532
AV3737 <vishwakarmaamit4000@gmail.com>
2025-02-27 12:53:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+// const fetchdata= async function fetchdata(){
+//   const pokemonname=document.getElementById("pokemonname").value;
+//   const response= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`);
+//   // console.log(response);
+//   const getdata=await response.json();
+//  const pokemonsprite=getdata.sprites.front_default;
+//  const imageElement= document.getElementById("Pokemonsprite")
+//  imageElement.src=pokemonsprite;
+//  imageElement.style.display="block";
+  
+// }
+// const fetchdata= async function fetchdata() {
+//   const pokemondata= document.getElementById("pokemonname").value;
+//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemondata}?limit=1000`);
+//   console.log(response);
+//   const data = await response.json();
+//   console.log(data);
+//   const pokemonsprite= data.sprites.front_default;
+//   const imageElement= document.getElementById("Pokemonsprite");
+//   imageElement.src=pokemonsprite;
+//   imageElement.style.display="block";
+// }
+// CODE:-
+// const start= async function start(){
+// const response= await fetch(`https://pokeapi.co/api/v2/pokemon`);
+// const data=await response.json();
+// console.log(data);
+
+// const pokemonsprite=data.sprites.front_default;
+// const image= document.getElementById("pokemonimage");
+// image.src=pokemonsprite;
+// image.style.display="block";
