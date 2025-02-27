43f9ae6e4cc2353ced3081e36b78ed3b36e4191d
AV3737 <vishwakarmaamit4000@gmail.com>
2025-02-27 12:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 
-// const fetchdata= async function fetchdata(){
-//   const pokemonname=document.getElementById("pokemonname").value;
-//   const response= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`);
-//   // console.log(response);
-//   const getdata=await response.json();
-//  const pokemonsprite=getdata.sprites.front_default;
-//  const imageElement= document.getElementById("Pokemonsprite")
-//  imageElement.src=pokemonsprite;
-//  imageElement.style.display="block";
+const fetchdata= async function fetchdata(){
+  const pokemonname=document.getElementById("pokemonname").value;
+  const response= await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonname}`);
+  // console.log(response);
+  const getdata=await response.json();
+ const pokemonsprite=getdata.sprites.front_default;
+ const imageElement= document.getElementById("Pokemonsprite")
+ imageElement.src=pokemonsprite;
+ imageElement.style.display="block";
   
-// }
+}
 // const fetchdata= async function fetchdata() {
 //   const pokemondata= document.getElementById("pokemonname").value;
 //   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemondata}?limit=1000`);
